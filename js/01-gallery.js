abd0440d19cdab7e9c237adf6289f1445f0256d2
Voytesha <elflowers.info@gmail.com>
2023-01-26 13:12:36 +0200

    first part done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description})=>{
+        return `<a class='gallery__link' href='${original}'>
+        <img  class='gallery__image' src ='${preview}' data-source='${original}' alt ='${description}'>
+        </a>`;}).join('');
+}
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e){
+    e.preventDefault();
+
+    if(e.target.nodeName !== 'IMG'){
+        return
+    }
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src='${e.target.dataset.source}'>
+    </div>
+`)
+
+instance.show()
+};
